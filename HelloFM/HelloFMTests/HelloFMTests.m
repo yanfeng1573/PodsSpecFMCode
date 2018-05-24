ae5403d45b9fca1bda334cf06353ea2c3fd6cb6d
yan feng liu <yanfeng@2c2p.com>
2018-05-24 20:10:04 +0800

    ModyOne

--- HelloFM/HelloFMTests/HelloFMTests.m
+++ HelloFM/HelloFMTests/HelloFMTests.m
@@ -1,39 +1,39 @@
+////
+////  HelloFMTests.m
+////  HelloFMTests
+////
+////  Created by yan feng liu on 24/5/18.
+////  Copyright © 2018 My2c2p. All rights reserved.
+////
 //
-//  HelloFMTests.m
-//  HelloFMTests
+//#import <XCTest/XCTest.h>
 //
-//  Created by yan feng liu on 24/5/18.
-//  Copyright © 2018 My2c2p. All rights reserved.
+//@interface HelloFMTests : XCTestCase
 //
-
-#import <XCTest/XCTest.h>
-
-@interface HelloFMTests : XCTestCase
-
-@end
-
-@implementation HelloFMTests
-
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+//@end
+//
+//@implementation HelloFMTests
+//
+//- (void)setUp {
+//    [super setUp];
+//    // Put setup code here. This method is called before the invocation of each test method in the class.
+//}
+//
+//- (void)tearDown {
+//    // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    [super tearDown];
+//}
+//
+//- (void)testExample {
+//    // This is an example of a functional test case.
+//    // Use XCTAssert and related functions to verify your tests produce the correct results.
+//}
+//
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+//
+//@end
